c65606d38427e96b776d463f9c4a9542086d954d
Siddharth Sahani <sidhucroedde@gmail.com>
2024-04-04 18:40:24 +0530

    feat: added state handlers

--- src/common.py
+++ src/common.py
@@ -1,14 +1,18 @@
 
 import pyglet
+from pyglet.window import Window, key, mouse
 
 
-display_window = pyglet.window.Window(
+display_window = Window(
     width=800, height=450,
     caption="TraffixSim"
 )
 
-clock = pyglet.clock.get_default()
-
-
 _window_icon = pyglet.image.load("assets/window_icon/window_icon(32x32).png")
 display_window.set_icon(_window_icon)
+
+
+key_handler = key.KeyStateHandler()
+mouse_handler = mouse.MouseStateHandler()
+
+display_window.push_handlers(key_handler, mouse_handler)
